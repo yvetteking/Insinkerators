e61593e2219744c33f6162b227af64ab9745de63
daleming <daleming@yahoo.com>
2016-11-24 12:19:11 -0600

    Add files via upload

--- Lazaro.h
+++ Lazaro.h
@@ -3,7 +3,50 @@
 
 using namespace std;
 
+void adjustScreen(int);
+
 void openTicket(){cout << "View an open ticket\n";}
-void closeTickets(){cout << "View a closed ticket\n";}
+
+
+/**************************************************************************
+
+Programmer: Dale Ming
+
+I placed closedTickets here just to show that the new vector for closed
+tickets is actually accepting ticket information correctly. Do with it
+whatever you like.
+
+You can see the output of this function (displaying closed tickets) when
+you choose #2 from the main menu.
+
+**************************************************************************/
+void closedTickets(vector<Tickets>& closedTicketLog)
+    {
+        int size = closedTicketLog.size();
+        int index = 0;
+        adjustScreen(12 - size);
+        while(index < size)
+            {
+                if (closedTicketLog[index].getTicketStatus() == "Closed")
+                    {
+                        cout << right << setw(10)
+                             << closedTicketLog[index].getTicketNumber() << "  "
+                             << closedTicketLog[index].getDate() << "  "
+                             << closedTicketLog[index].getApt() << "  " << left << setw(10)
+                             << closedTicketLog[index].getIssue() << "  "
+                             << closedTicketLog[index].getNotes() << endl;
+                        ++index;
+                    }
+                else ++index;
+            }
+            adjustScreen(3);
+    }
+
+
 
 #endif // LAZARO_H_INCLUDED
+
+
+
+
+
